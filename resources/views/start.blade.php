97f1f5c2865dd103658174e7db48e062e3b071bb
Escudo7 <permswolf@ys.ru>
2019-09-10 21:34:23 +0500

    add layouts

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -1,15 +1,1 @@
-<!doctype html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <link rel="stylesheet" 
-            href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
-            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
-            crossorigin="anonymous">
-    </head>
-    <body>
-        <nav class="navbar">
-            <a class="navbar-brand" href="#">Pages Parser</a>
-        </nav>
-    </body>
-</html>
+@extends('layouts.main')

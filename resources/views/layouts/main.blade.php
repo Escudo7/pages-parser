6863af7b6dcb8c8bf6da4b834d6ebc19aef1f02a
Escudo <permswolf@yandex.ru>
2019-10-04 21:57:13 +0200

    update navbar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,17 +10,14 @@
     </head>
     <body>
         @yield('content')
-        <nav class="navbar" style="background-color: #e3f2fd;">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+            <div class="navbar-collapse">
                 <ul class="navbar-nav">
                     <li class="nav-item active">
                         <a class="nav-link" href="{{ route('domains.create') }}">Pages Parser</a>
                     </li>
                     <li class="nav-item active">
-                        <a class="nav-link" href="{{ route('domains.index') }}">List</a>
+                        <a class="nav-link" href="{{ route('domains.index') }}">Domain List</a>
                     </li>
                 </ul>
             </div>

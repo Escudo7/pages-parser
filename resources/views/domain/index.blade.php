00e68e1252a7f8ef2b62fb49060552ab433147a1
Escudo <permswolf@yandex.ru>
2019-10-04 20:35:10 +0200

    add bootstrap-table

--- resources/views/domain/index.blade.php
+++ resources/views/domain/index.blade.php
@@ -1,13 +1,15 @@
 @extends('layouts.main')
 
 @section('content')
-    <table>
-        <tr>
-            <th>ID</th>
-            <th>Name</th>
-        </tr>
-        @foreach($domains as $domain)
+    <table class='table table-bordered table-hover table-sm'>
+        <thead class='thead-dark'>
             <tr>
+                <th>ID</th>
+                <th>Name</th>
+            </tr>
+        </thead>
+        @foreach($domains as $domain)
+            <tr class='table-info'>
                 <td>{{ $domain->id }}</td>
                 <td>{{ $domain->name }}</td>
             </tr>
--- resources/views/domain/show.blade.php
+++ resources/views/domain/show.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.main')
 
 @section('content')
-    <table>
-        <tr>
-            <th>ID</td>
-            <th>Name</td>
-        </tr>
-        <tr>
+    <table class='table table-bordered table-hover table-sm'>
+        <thead class='thead-dark'>
+            <tr>
+                <th>ID</td>
+                <th>Name</td>
+            </tr>
+        </thead>
+        <tr class='table-info'>
             <td>{{ $domain->id }}</td>
             <td>{{ $domain->name }}</td>
         </tr>

e7e815d1f2b8f3609bb0ed0949caae296bd36e4f
Escudo <permswolf@yandex.ru>
2019-10-23 21:22:21 +0200

    update testing

--- tests/DomainControllerTest.php
+++ tests/DomainControllerTest.php
@@ -6,55 +6,58 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Jobs\PageParserJob;
 
-class PagesTest extends TestCase
+class DomainControllerTest extends TestCase
 {
     use DatabaseMigrations;
     use DatabaseTransactions;
 
     public function testStartPages()
     {
-        $this->assertEquals(0, DB::table('domains')->count());
-        $response = $this->call('GET', route('domains.create'));
-        $this->assertEquals(200, $response->status());
+        $this->get(route('domains.create'));
+        $this->seeStatusCode(200);
     }
 
-    public function testDomainsStoreWithCorrectRequest()
+    public function testStore()
     {   
         $this->assertEquals(0, DB::table('domains')->count());
         $data = ['pagesAdress' => 'http://lumen.laravel.com'];
-        $response = $this->call('POST', route('domains.store'), $data);
+        $this->post(route('domains.store'), $data);
         $this->seeStatusCode(302);
         $this->assertEquals(1, DB::table('domains')->count());
         $this->seeInDatabase('domains', ['name' => 'http://lumen.laravel.com']);
     }
 
-    public function testDomainsStoreWithIncorrectRequest()
+    public function testStoreWithIncorrectRequest()
     {
         $this->assertEquals(0, DB::table('domains')->count());
         $data = ['pagesAdress' => 'lumen.laravel.com'];
-        $response = $this->call('POST', route('domains.store'), $data);
+        $this->post(route('domains.store'), $data);
         $this->seeStatusCode(302);
         $this->assertEquals(0, DB::table('domains')->count());
     }
 
-    public function testDomainsIndex()
+    public function testShow()
     {
-        $this->assertEquals(0, DB::table('domains')->count());
-        $data1 = ['pagesAdress' => 'http://lumen.laravel.com'];
-        $data2 = ['pagesAdress' => 'http://fantlab.ru'];
-        $this->call('POST', route('domains.store'), $data1);
-        $this->call('POST', route('domains.store'), $data2);
-        $this->assertEquals(2, DB::table('domains')->count());
-        $response = $this->call('GET', route('domains.index'));
-        $this->assertEquals(200, $response->status());
+        $domain = new \App\Domain();
+        $domain->name = 'http://lumen.laravel.com';
+        $domain->save();
+        $id = $domain->id;
+        $this->get(route('domains.show', ['id' => $id]));
+        $this->seeStatusCode(200);
     }
 
-    public function testPegeParserJob()
+    public function testIndex()
+    {
+        $this->get(route('domains.index'));
+        $this->seeStatusCode(200);
+    }
+
+    public function testParserJob()
     {
         $clientName = 'testClient';
         $data1 = ['pagesAdress' => 'http://lumen.laravel.com'];
         $this->assertEquals(0, DB::table('domains')->count());
-        $this->call('POST', route('domains.store'), $data1);
+        $this->post(route('domains.store'), $data1);
         $id = DB::table('domains')->max('id');
         dispatch(new PageParserJob($data1['pagesAdress'], $id, $clientName));
         $this->assertEquals(1, DB::table('domains')->count());
